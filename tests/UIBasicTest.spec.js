a8b17508204352ce115fa2a225c8b978f5b80a52
Abdul-Razak  Hussein <abdul-razak.hussein@amalitech.com>
2023-12-18 16:52:05 +0000

    add homepage test script

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -3,7 +3,7 @@ const {test, expect} = require('@playwright/test');
 
 const name = "rahulshettyacademy";
 const password = "learning";
-test.only('Login test', async ({page}) => {
+test.only('home/landing page test', async ({page}) => {
     const userName = page.locator("#username");
     const Userpassword = page.locator("[type='password']");
     const signIn = page.locator("#signInBtn");
@@ -14,14 +14,27 @@ test.only('Login test', async ({page}) => {
     await page.locator("#terms").check();
     await expect(page.locator("#terms")).toBeChecked();
     await signIn.click();
+    await page.pause();
+ 
+    
+
+});
+test('Login test', async ({page}) => {
+    const userName = page.locator("#username");
+    const Userpassword = page.locator("[type='password']");
+    const signIn = page.locator("#signInBtn");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+    console.log(await page.title());
+    await userName.fill("rahulshetty");
+    await Userpassword.fill("learning");
+    await page.locator("#terms").check();
+    await expect(page.locator("#terms")).toBeChecked();
+    await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toHaveText("Incorrect username/password.");
     await expect(page.locator("[style*='block']")).toContainText("Incorrect");
-
-    // console.log(await page.title());
-    // await expect(page).toHaveTitle("Google");
-    // await page.pause();
-    // // await new Promise(() => {});
+    await page.pause();
+ 
     
 
 });
