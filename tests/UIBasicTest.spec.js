585d0cb74a19d8bd9888f4fd4af46ea62e7b0426
Abdul-Razak  Hussein <abdul-razak.hussein@amalitech.com>
2023-12-18 16:17:46 +0000

    make use of basic assertions

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -2,32 +2,22 @@
 const {test, expect} = require('@playwright/test');
 
 
-// test('Browser context playwright test', async ({browser}) =>
-// {
-
-//     const context = browser.newContext();
-//     const page = await context.newPage();
-//     await page.goto("https://rahulshettyacademy.com/loginpagePractise");
-//     console.log(await page.title());
-
-// });
 test.only('Login test', async ({page}) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise");
     console.log(await page.title());
     await page.locator('#username').fill("rahulshetty");
     await page.locator("[type='password']").fill("learning");
+    await page.locator("#terms").check();
+    await expect(page.locator("#terms")).toBeChecked();
     await page.locator("#signInBtn").click();
-    await page.pause();
-    //await new Promise(() => {});
-})
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toHaveText("Incorrect username/password.");
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 
-test('Page Playwright test', async ({page}) =>
-{
-    await page.goto("https://google.com");
-    console.log(await page.title());
-    await expect(page).toHaveTitle("Google");
-    await page.pause();
-    // await new Promise(() => {});
+    // console.log(await page.title());
+    // await expect(page).toHaveTitle("Google");
+    // await page.pause();
+    // // await new Promise(() => {});
     
 
 });
