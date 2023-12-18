ea65440702738c3a4e954fc442319f9cb962990c
Abdul-Razak  Hussein <abdul-razak.hussein@amalitech.com>
2023-12-18 16:40:29 +0000

    refactoring to exclude the data from the test script

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -1,15 +1,19 @@
 // @ts-check
 const {test, expect} = require('@playwright/test');
 
-
+const name = "rahulshettyacademy";
+const password = "learning";
 test.only('Login test', async ({page}) => {
+    const userName = page.locator("#username");
+    const Userpassword = page.locator("[type='password']");
+    const signIn = page.locator("#signInBtn");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise");
     console.log(await page.title());
-    await page.locator('#username').fill("rahulshetty");
-    await page.locator("[type='password']").fill("learning");
+    await userName.fill(name);
+    await Userpassword.fill(password);
     await page.locator("#terms").check();
     await expect(page.locator("#terms")).toBeChecked();
-    await page.locator("#signInBtn").click();
+    await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toHaveText("Incorrect username/password.");
     await expect(page.locator("[style*='block']")).toContainText("Incorrect");
